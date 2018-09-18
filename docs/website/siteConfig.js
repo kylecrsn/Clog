768a5ccc6420f17ed59bc175d00cf68ac82dbf9d
carsonkk <carsonkk@gmail.com>
2018-09-18 10:55:54 -0700

    circleci testing

--- docs/website/siteConfig.js
+++ docs/website/siteConfig.js
@@ -22,7 +22,7 @@ const users = [
 
 const siteConfig = {
   title: 'Clog', // Title for your website.
-  tagline: 'A website for testing',
+  tagline: 'A simple logging library for C',
   url: 'https://carsonkk.github.io', // Your website URL
   baseUrl: '/Clog/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
@@ -73,7 +73,7 @@ const siteConfig = {
   */
 
   // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
-  copyright: `Copyright © ${new Date().getFullYear()} Your Name or Your Company Name`,
+  copyright: `Copyright © ${new Date().getFullYear()} Kyle Carson`,
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks.
