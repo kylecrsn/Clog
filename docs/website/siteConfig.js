1b867315f9663e7b1e27c8d70097b22889793ca4
carsonkk <carsonkk@gmail.com>
2018-09-17 17:47:27 -0700

    testing travis doc publishing

--- docs/website/siteConfig.js
+++ docs/website/siteConfig.js
@@ -21,16 +21,16 @@ const users = [
 ];
 
 const siteConfig = {
-  title: 'Test Site', // Title for your website.
+  title: 'Clog', // Title for your website.
   tagline: 'A website for testing',
   url: 'https://carsonkk.github.io', // Your website URL
-  baseUrl: '/clog/', // Base URL for your project */
+  baseUrl: '/Clog/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'clog',
+  projectName: 'Clog',
   organizationName: 'carsonkk',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
